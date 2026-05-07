d058e0fec9f7c7731b2cfe278330b495939222c1
Kirthi7924 <keerthiprakash7777@gmail.com>
2026-05-07 16:01:55 +0530

    Add files via upload

---
+++ app/src/test/java/com/aountech/heartdiseasesprediction/main/java/com/aountech/heartdiseasesprediction/utilities/OnRvItemClickListner.java
@@ -0,0 +1,8 @@
+package com.aountech.heartdiseasesprediction.utilities;
+
+import com.aountech.heartdiseasesprediction.models.Patient;
+
+public interface OnRvItemClickListner {
+    void onItemClicked(Patient patient,int position);
+
+}
